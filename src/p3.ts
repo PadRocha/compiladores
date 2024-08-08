4851377c0981a050487e9767d8d590d0f7c8dd81
padrocha <p.roch@outlook.es>
2024-08-08 16:10:57 -0600

    translate

--- src/p3.ts
+++ src/p3.ts
@@ -1,9 +1,9 @@
 class Translate {
-  public ph_map: Map<string, string>;
+  public tokens: Map<string, string>;
   private token_count: number;
 
   constructor(public line: string) {
-    this.ph_map = new Map();
+    this.tokens = new Map();
     this.token_count = 0;
   }
 
@@ -20,6 +20,7 @@ class Translate {
 
     for (let i = line.length - 1; i >= 0; i--) {
       const char = line[i];
+      if (/\s/.test(char)) continue;
       if (char === char_e) {
         if (!flag) {
           flag = true;
@@ -38,7 +39,7 @@ class Translate {
             const start = i + (is_root ? 1 : 0);
             line = line.slice(0, start) + token + line.slice(index);
             accumulator = this.transform(accumulator);
-            this.ph_map.set(token, accumulator);
+            this.tokens.set(token, accumulator);
           }
           flag = false;
           is_digit = true;
@@ -46,7 +47,7 @@ class Translate {
         }
       } else if (flag) {
         accumulator = char + accumulator;
-        if (!/[\d\w&]/.test(char)) {
+        if (!/[&\d\w]/.test(char)) {
           is_digit = false;
         }
       }
@@ -61,8 +62,9 @@ class Translate {
 
   private translate(line: string): string {
     let flag = false;
+    let radical = false;
     let accumulator = "";
-    let op = "";
+    let op = 0;
     let aux = "";
     let lhs = "";
     let rhs = "";
@@ -70,11 +72,47 @@ class Translate {
 
     for (let i = 0; i < line.length; i++) {
       const char = line[i];
-      if (char === "\\" && !flag) {
+      if (/\s/.test(char)) continue;
+      // if (/[+\-*:\\/]/.test(char) && !flag) {
+      //   lhs = "";
+      //   flag = true;
+      // } else {
+      //   lhs += char;
+      // }
+
+      if (flag) {
+        if (radical) {
+          if (i == line.length - 1 || /[+\-*:^]/.test(char)) {
+            if (i == line.length - 1) {
+              rhs += char;
+            }
+            if (lhs === "2") {
+              accumulator += `sqrt(${rhs})`;
+            } else {
+              accumulator += `pow(${rhs}, 1 / (${lhs}))`;
+            }
+            flag = false;
+            radical = false;
+            lhs = "";
+            rhs = "";
+            if (/[+\-*:^]/.test(char)) {
+              accumulator += char;
+            }
+          } else {
+            console.log("reading rhs");
+            rhs += char;
+          }
+        } else if (char === "/") {
+          radical = true;
+        } else {
+          lhs += char;
+        }
+      } else if (char === "\\") {
         flag = true;
-        op = "sqrt";
-        aux = "*";
+      } else {
+        accumulator += char;
       }
+
       // if (/[&\d\w]/.test(char)) {
       //   if (/[^&\d]/.test(char)) {
       //     is_token = false;
@@ -91,9 +129,7 @@ class Translate {
       // }
     }
 
-    console.log(lhs, rhs, is_token);
-
-    return line;
+    return accumulator;
   }
 
   public get converted(): string {
@@ -103,9 +139,10 @@ class Translate {
 
 (async () => {
   // const line = `31^n*(\\(23*3)/3+(9*8))*8-(3*8)*\\8-8/32`;
-  const line = `\\3+8/(a*8-8)`;
-  console.log("Texto original:", line);
+  // const line = `8 + \\3 + 8/(a * 8 - 8) + 8`;
+  const line = "\\2/\\2/3+\\4/3";
+  console.log("Original:", line);
   const conv = new Translate(line);
-  console.log("Texto con tokens:", conv.converted);
-  console.log(conv.ph_map);
+  console.log("Traducido:", conv.converted);
+  console.log(conv.tokens);
 })();
